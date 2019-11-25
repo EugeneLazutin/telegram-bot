961cb7a07f7efa85d5be8478ea2a15b689e58d64
Eugeny Lazutin <eugeny.lazutin@itechart-group.com>
2019-11-25 14:52:54 +0300

    add telegraf; test deploy

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const getToken = require('./auth');
+const fs = require('fs');
+const Telegraf = require('telegraf');
+const {getCredentials, authorize} = require('./auth');
 const writeValue = require('./googleApi');
 
-getToken((token) => {
-    writeValue(token, '1c2q8foww7nhmPODR9ehpA7K1OBvpxxei0tCgy-JvBp4', 'Лазутин Евгений', 'да')
-});
+function start(token) {
+    console.log('bot token - ', token);
+    const bot = new Telegraf(token);
+
+    bot.start((ctx) => ctx.reply('Welcome'));
+    bot.help((ctx) => ctx.reply('Send me a sticker'));
+    bot.on('sticker', (ctx) => ctx.reply('👍'));
+    bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+    return bot.launch();
+}
+
+if(process.env.bot_token) {
+    start(process.env.bot_token);
+} else {
+    fs.readFile('bot_token.json', 'utf8', (err, content) => {
+        if (err) return console.log('Error loading bot token file:', err);
+        start(content);
+    });
+}
+
+// getCredentials().then(authorize).then((token) => {
+//     writeValue(token, '1c2q8foww7nhmPODR9ehpA7K1OBvpxxei0tCgy-JvBp4', 'Лазутин Евгений', 'да');
+// });
